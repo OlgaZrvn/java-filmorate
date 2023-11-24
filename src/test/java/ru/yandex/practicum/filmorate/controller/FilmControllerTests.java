1add045f43f34ea155fc6c07466a405a18acf314
Ольга <olga.zrvn@yandex.ru>
2023-11-24 20:31:28 +0500

    TЗ № 9 v6

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTests.java
@@ -1,10 +1,9 @@
-package ru.yandex.practicum.filmorate.сontroller;
+package ru.yandex.practicum.filmorate.controller;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.ValidationException;
-import ru.yandex.practicum.filmorate.controller.FilmController;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.time.LocalDate;
@@ -12,6 +11,7 @@ import java.time.LocalDate;
 public class FilmControllerTests {
 
     private FilmController filmController;
+
     @BeforeEach
     public void setUp() {
         filmController = new FilmController();
@@ -23,6 +23,7 @@ public class FilmControllerTests {
                 LocalDate.of(1999, 12, 28), 90L);
         Assertions.assertThrows(ValidationException.class, () -> filmController.postFilm(film));
     }
+
     @Test
     public void shouldNotAddDescriptionLength201() {
         Film film = new Film(0, "Титаник", "В первом и последнем плавании шикарного «Титаника» " +
@@ -31,6 +32,7 @@ public class FilmControllerTests {
                 LocalDate.of(1997, 11, 1), 194L);
         Assertions.assertThrows(ValidationException.class, () -> filmController.postFilm(film));
     }
+
     @Test
     public void shouldAddDescriptionLength200() {
         Film film = new Film(0, "Титаник", "В первом и последнем плавании шикарного «Титаника» " +
@@ -39,6 +41,7 @@ public class FilmControllerTests {
                 LocalDate.of(1997, 11, 1), 194L);
         filmController.postFilm(film);
     }
+
     @Test
     public void shouldNotAddDate1797() {
         Film film = new Film(0, "Титаник", "Desc",
